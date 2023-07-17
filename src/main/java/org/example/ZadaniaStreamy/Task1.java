f46d10a18b724a4b59e79618e730225fc77a27b7
Przemek Koscinski <przem.koscinski@gmail.com>
2023-07-17 21:53:01 +0200

    Task1 in Streams

--- src/main/java/org/example/ZadaniaStreamy/Task1.java
+++ src/main/java/org/example/ZadaniaStreamy/Task1.java
@@ -1,4 +1,41 @@
 package org.example.ZadaniaStreamy;
 
+import java.util.Scanner;
+
+//Odwróć liczbę za pomocą pętli while.
+//        Program poprosi użytkownika o wprowadzenie numeru, a następnie odwróci liczbę za
+//        pomocą pętli while.
+
+
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Long liczba = scanner.nextLong();
+
+        String liczbaString = liczba.toString();
+        char[] liczbaChar = liczbaString.toCharArray();
+        char[] reversedLiczba = new char[liczbaChar.length];
+
+        int counter = liczbaChar.length;
+        int reverseCounter = 0;
+
+        while(counter!=0){
+
+            reversedLiczba[reverseCounter]=liczbaChar[counter-1];
+            reverseCounter++;
+            counter--;
+        }
+
+        int number = Integer.parseInt(new String(reversedLiczba));  // po odwroceniu tablicy rzutujemy ja na int, żeby moc wykonywac operacje na odwroconej liczbie
+        System.out.println(number);
+
+        int nowaLiczba = number +2;
+        System.out.println(nowaLiczba);
+
+
+
+    }
 }
